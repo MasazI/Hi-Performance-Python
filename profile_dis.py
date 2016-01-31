2e3de4151bd35b944c8a32e4d8f7818cc9df6e6d
masazdream <masaz.dream@gmail.com>
2016-01-31 15:13:02 +0900

    add profile usind dis module.

---
+++ profile_dis.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+def fn_expressive(upper = 1000000):
+    total = 0
+    for n in xrange(upper):
+        total += n
+    return total
+
+
+def fn_terse(upper = 1000000):
+    return sum(xrange(upper))
+
+print("同じ計算結果になる: %s" % (fn_expressive() == fn_terse()))
