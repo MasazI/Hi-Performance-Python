dddc8591395c99754123a845c8fb30001b46c50d
masazdream <masaz.dream@gmail.com>
2016-02-06 12:35:51 +0900

    組み込みの関数を追加

--- list_tuple.py
+++ list_tuple.py
@@ -12,7 +12,16 @@ def linear_search(needle, array):
             return 1
     return -1
 
+def linear_search_embed(needle, array):
+    try:
+        index = array.index(needle)
+    except Exception as e:
+        return -1
+    return index
 
 if __name__ == '__main__':
     print linear_search(1, [4,5,6,7,8])
     print linear_search(2, [3,4,5,2,5])
+
+    print linear_search_embed(1, [4,5,6,7,8])
+    print linear_search_embed(2, [3,4,5,2,5])
