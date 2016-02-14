f832575967181583dd42a89d7e8a2b95b7fa36ea
masazdream <masaz.dream@gmail.com>
2016-02-14 17:45:49 +0900

    ハッシュ関数をもったクラスのサンプルを追加

--- dict_set.py
+++ dict_set.py
@@ -14,7 +14,25 @@ def index_sequence(key, mask=0b111, PERTURB_SHIFT=5):
         perturb >>= PERTURB_SHIFT
         yield i & mask
 
-if __name__ == '__main__':
+
+def create_index():
+    '''
+    index作成のサンプル
+    '''
     for i in index_sequence('abc'):
         # 辞書のインデックスを探索して空かどうか確認する:
         print i
+
+
+class City(str):
+    def __hash__(self):
+        return ord(self[0])
+
+if __name__ == '__main__':
+    data = {
+        City("Rome"): 4,
+        City("San Francisco"): 3,
+        City("New Your"): 5,
+        City("Barcelona"): 2
+    }
+    print data
