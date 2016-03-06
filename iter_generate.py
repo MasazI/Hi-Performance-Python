1e20e378fbcffe2f93e39a6838c5ebbd2b346a98
masazdream <masaz.dream@gmail.com>
2016-03-06 23:53:56 +0900

    フィボナッチ数列を例に、データの生成と変換フェーズのコードを追加

--- iter_generate.py
+++ iter_generate.py
@@ -56,3 +56,15 @@ def fibonacci():
     while True:
         yield j
         i, j = j, i + j
+
+def fibonacci_transform():
+    count = 0
+    for f in fibonacci():
+        if f > 5000:
+            break
+        if f % 2:
+            count += 1
+    return count
+
+print fibonacci_transform()
+
